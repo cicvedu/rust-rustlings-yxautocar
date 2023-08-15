55f453d404ed3606cfbc3195f15acefa1195f297
yxautocar <648198949@qq.com>
2023-08-15 16:25:56 +0800

    anything

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -10,9 +10,9 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = Vec::new( );
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -22,7 +22,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = vec.clone( );
 
     vec.push(22);
     vec.push(44);
